2a4244ff1a54e16b87b4a0119cfaad6ec6568025
shp <1328445041@qq.com>
2018-04-02 00:34:50 +0800

    build project

---
+++ database-api/src/main/java/com/shp/service/UserService.java
@@ -0,0 +1,10 @@
+package com.shp.service;
+
+import com.shp.model.User;
+
+/**
+ * Created by Shape on 2018/4/1.
+ */
+public interface UserService {
+    User getUserByName(String name);
+}
---
+++ database-impl/src/main/java/com/shp/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.shp.serviceImpl;
+
+import com.shp.model.User;
+import com.shp.service.UserService;
+
+/**
+ * Created by Shape on 2018/4/2.
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUserByName(String name) {
+        return null;
+    }
+}
